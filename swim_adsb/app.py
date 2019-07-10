5540ac225786d75be46fe8c3ac1a839a5a3be02b
Alex Ntavelos <a.ntavelos@gmail.com>
2019-07-10 16:49:34 +0200

    read username password from config

--- swim_adsb/app.py
+++ swim_adsb/app.py
@@ -67,8 +67,8 @@ for city, code in config['CITIES'].items():
     flights.create_topic(id=f"departures.{city.lower()}", callback=departures_handler)
 
 # create publisher based on a real user of Subscription Manager and assign the topic group on them
-publisher = app.register_publisher(username=os.environ['SWIM_ADSB_USERNAME'],
-                                   password=os.environ['SWIM_ADSB_PASSWORD'])
+publisher = app.register_publisher(username=app.config['SWIM_ADSB_USERNAME'],
+                                   password=app.config['SWIM_ADSB_PASSWORD'])
 publisher.register_topic_group(flights)
 
 
